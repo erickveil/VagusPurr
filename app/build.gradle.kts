b8a762d3758c1fd352e379c19c1e26b76c9efaba
eveil <erick@erickveil.net>
2024-07-08 12:49:03 -0700

    Updated versioning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "net.erickveil.vaguspurr"
         minSdk = 33
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- changelog.md
+++ changelog.md
@@ -4,8 +4,8 @@ v 1,9
 
 First release!
 
-v 1.1
+v 1.0.1
 
 - Fixed light text in light theme so that it is more visible in both light and dark themes.
-- Added version info to manifest.
+- Updated versioning in gradle
 
